52a62baab78a34c194fe9e074dfd85b2295a3acb
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-02-21 23:15:05 -0600

    AHORA SI, THIS.LOCATION.BACK

--- src/app/Guards/login.guard.ts
+++ src/app/Guards/login.guard.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+  constructor(private location: Location) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -14,6 +16,7 @@ export class LoginGuard implements CanActivate {
 
       if(token) {
         alert('Ya estás logueado.');
+        this.location.back();
         return false;
       }
       return true;
